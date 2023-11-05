ab9d8c2d69f59ed4888960b58ef35da59709cb90
AlaaHamayde <alaahamayde2222000@gmail.com>
2023-11-05 13:01:52 +0300

    modified task2

--- Automation_SQL/backup_SQL.sh
+++ Automation_SQL/backup_SQL.sh
@@ -2,14 +2,15 @@
 
 
 TIMESTAMP=$(date "+%Y%m%d")
-DIR="BACKUP_FILE"
-read -p "Enter MySQL username: " MYSQL_USER
-read -p "Enter MySQL password: " MYSQL_PASSWORD
-read -p "Enter MySQL host  " MYSQL_HOST
-MYSQL_HOST=${MYSQL_HOST:-localhost} # Use 'localhost' if user doesn't enter a value
-read -p "Enter MySQL port (default: 3306): " DB_PORT
-DB_PORT=${DB_PORT:-3306} # Use 3306 if user doesn't enter a value
-read -p "Enter the name of the database to back up: " DB_NAME
+DIR="TrainingRepo/Automation_SQL/BACKUP_FILE"
+ MYSQL_USER="root"
+ MYSQL_PASSWORD="root"
+ MYSQL_HOST="localhost"
+#MYSQL_HOST=${MYSQL_HOST:-localhost} # Use 'localhost' if user doesn't enter a value
+#read -p "Enter MySQL port (default: 3306): " DB_PORT
+DB_PORT=3306
+#read -p "Enter the name of the database to back up: "
+ DB_NAME="store"
 # Check if the directory exists
 if [ ! -d "$DIR" ]; then
     
@@ -18,7 +19,7 @@ if [ ! -d "$DIR" ]; then
 else
     echo "Directory already exists: $DIR"
 fi
-BACKUP="BACKUP_FILE/backup-${TIMESTAMP}.sql"
+BACKUP="$DIR/backup-${TIMESTAMP}.sql"
 sudo mysqldump --user=$MYSQL_NAME --password=$MYSQL_PASSWORD --host=$MYSQL_HOST --port=$DB_PORT $DB_NAME > $BACKUP
 
 # check if the backup file exist 
